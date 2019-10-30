d5c9a97767fbec94330e35e4016f85304ebff38e
Nicolas Kepper <30436699+Cionco@users.noreply.github.com>
2019-10-30 12:16:19 +0100

    Added invalidateObject Method
    changed processes in a minor way
    renamed ids
    removed passportNumber from User
    added some structure

--- Praxisprojekt.sol
+++ Praxisprojekt.sol
@@ -6,7 +6,7 @@ contract Praxisprojekt {
   uint userCount = 0;
 
   struct Institution {
-    uint institutionId;
+    uint id;
     string name;
     string email;
     bytes32 password;
@@ -14,18 +14,17 @@ contract Praxisprojekt {
   }
 
   struct Object {
-    uint documentId;
+    uint id;
     string description;
     bytes32 signature;
     uint256 timestamp;
-    string documentContent;
+    string link; //Where the object is actually stored. either full url or just path on the file system, probably depending on the used protocol
   }
 
   struct User {
-    uint userId;
+    uint id;
     string firstName;
     string lastName;
-    string passportNumber;
     string email;
     bytes32 password;
     string caCertificate;
@@ -36,38 +35,31 @@ contract Praxisprojekt {
   mapping(address => User) users;
 
   function createInstitution(string memory name, string memory email, string memory password) public returns(bool){
-    if(!(institutions[msg.sender].institutionId > 0)) {
+    if(!(institutions[msg.sender].id > 0)) {
         institutions[msg.sender] = Institution(institutionCount++, name, email, sha256(abi.encode(password)), "");
         return true;
     }
     return false;
   }
 
-  function createUser(string memory firstName, string memory lastName, string memory passportNumber, string memory email, string memory password, string memory caCertificate) public returns(bool){
-    if(!(users[msg.sender].userId > 0)) {
-        users[msg.sender] = User(userCount++, firstName, lastName, passportNumber, email, sha256(abi.encode(password)), caCertificate);
+// *************************************** USER *******************************************************************
+  function createUser(string memory firstName, string memory lastName, string memory email, string memory password, string memory caCertificate) public returns(bool){
+    if(!(users[msg.sender].id > 0)) {
+        users[msg.sender] = User(userCount++, firstName, lastName, email, sha256(abi.encode(password)), caCertificate);
         return true;
     }
     return false;
   }
-
-  function createObject(string memory description, string memory documentContent) public returns(bool) {
-    if(streql(getObject(description), "")) {
-      objects[msg.sender].push(Object(objectCount++, description, keccak256(abi.encode(documentContent)), now, documentContent));
-      return true;
-    }
-    return false;
+  
+  function authenticate(string memory user, string memory password) public view returns(bool) {
+  	return streql(user, users[msg.sender].email) && sha256(abi.encode(password)) == users[msg.sender].password;
   }
-
+  
   function getUserFirstName() public view returns (string memory) {
     return users[msg.sender].firstName;
   }
   function getUserLastName() public view returns (string memory) {
     return users[msg.sender].lastName;
-  }
-
-  function getUserPassportNumber() public view returns (string memory) {
-    return users[msg.sender].passportNumber;
   }
 
   function getUserEmail() public view returns (string memory) {
@@ -81,28 +73,62 @@ contract Praxisprojekt {
     return users[msg.sender].caCertificate;
   }
 
-  function getObject(string memory description) public view returns(string memory) {
-  	uint objects_length = objects[msg.sender].length;
-    for(uint i = 0; i < objects_length; i++) {
-    	if(streql(objects[msg.sender][i].description, description)) return objects[msg.sender][i].documentContent;
-    }
+// **********************************************************************************************************
+  
+// *************************************** Object *******************************************************************
+  function getObjectLink(string memory description) public view returns(string memory) {
+    return getObject(description).link;
   }
 
-  function getObjectById(uint document_id) public view returns(string memory, string memory) {
-  	Object memory o = objects[msg.sender][document_id];
-    return (o.description, o.documentContent);
+  function createObject(string memory description, string memory link) public returns(bool) {
+    if(streql(getObjectLink(description), "")) {
+      objects[msg.sender].push(Object(objectCount++, description, keccak256(abi.encode(link)), now, link));
+      return true;
+    }
+    return false;
+  }
+
+  //Returns the n'th (user_document_id'th) object of the sender
+  function getObjectById(uint user_document_id) public view returns(string memory, string memory) {
+  	Object memory o = objects[msg.sender][user_document_id];
+    return (o.description, o.link);
   }
 
   function getObjectCount() public view returns(uint) {
   	return objects[msg.sender].length;
   }
 
+  //Deletes the object from the list but seems to keep the array length the same as before
+  function invalidateObject(string memory description) public returns(bool) {
+    int index = findObject(description);
+    if(index < 0) return false;
+    
+    for(uint j = uint(index); j < objects[msg.sender].length - 1; j++) {
+        objects[msg.sender][j] = objects[msg.sender][j + 1];
+    }
+    delete objects[msg.sender][objects[msg.sender].length - 1];
+    objects[msg.sender].length--;
+  }
+// **********************************************************************************************************
+
+  //Returns the object to a description. Not public because I don't know how structs are published to an outside referrer
+  function getObject(string memory description) private view returns(Object memory) {
+    uint objects_length = objects[msg.sender].length;
+    for(uint i = 0; i < objects_length; i++) {
+    	if(streql(objects[msg.sender][i].description, description)) return objects[msg.sender][i];
+    }
+  }
+  
+  //Returns the index of the object with this description, -1 if none exists
+  function findObject(string memory description) private view returns(int) {
+      uint objects_length = objects[msg.sender].length;
+      for(uint i = 0; i < objects_length; i++) {
+          if(streql(objects[msg.sender][i].description, description)) return int(i);
+      }
+      return -1;
+  }
 
   function streql(string memory arg1, string memory arg2) private pure returns(bool) {
     return keccak256(abi.encode(arg1)) == keccak256(abi.encode(arg2));
-  }
-
-  function authenticate(string memory user, string memory password) public view returns(bool) {
-  	return streql(user, users[msg.sender].email) && sha256(abi.encode(password)) == users[msg.sender].password;
   }
 }
